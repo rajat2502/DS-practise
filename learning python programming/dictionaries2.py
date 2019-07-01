609b08db266b816b72c0102e8e829287c0a6a642
abc <rajat2502@users.noreply.github.com>
2019-07-01 23:37:57 +0530

    learned to access elements of a dictionary

---
+++ learning python programming/dictionaries2.py
@@ -0,0 +1,77 @@
+# access a ey from a dictionary
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+print(zodiac_elements["earth"])
+print(zodiac_elements["fire"])
+
+#it is a good practice to check before if that element exixt or not so that it will not throw an error
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3}
+key_to_check = "Landmark 81"
+
+if key_to_check in building_heights:
+  print(building_heights["Landmark 81"])
+
+#Try/Except to Get a Key
+caffeine_level = {"espresso": 64, "chai": 40, "decaf": 0, "drip": 120}
+
+try: 
+  print(caffeine_level["matcha"])
+except:
+  print("Unknown Caffeine Level")
+
+#.get() method
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+
+tc_id = user_ids.get("teraCoder", 100000)
+print(tc_id)
+stack_id = user_ids.get("superStackSmash", 100000)
+print(stack_id)
+
+
+#.pop() method
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+
+health_points += available_items.pop("stamina grains", 0)
+health_points += available_items.pop("power stew", 0) 
+health_points += available_items.pop("mystic bread", 0) 
+print(available_items)
+print(health_points)
+
+#get all keys
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+users = user_ids.keys()
+lessons = num_exercises.keys()
+
+print(users)
+print(lessons)
+
+#get all values by .values() method
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0
+
+for value in num_exercises.values():
+  total_exercises += value
+  
+print(total_exercises)
+
+#get all items
+
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for key, value in pct_women_in_occupation.items():
+  print("Women make up "+ str(value) +" percent of " + key + "s.")
+
+#practice
+  tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+spread = {}
+spread["past"] = tarot.pop(13)
+spread["present"] = tarot.pop(22)
+spread["future"] = tarot.pop(10)
+
+for key, value in spread.items():
+	print("Your " + key +" is the " + value + " card.")
